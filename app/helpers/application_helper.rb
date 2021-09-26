caf4ad596fcd6a693bbc2123ba997b8a345b88bd
Sean Wangnick <swangnick@gmail.com>
2021-09-26 14:27:44 -0500

    Update Project Name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def page_title(separator = " - ")
+        [content_for(:title), 'BookCollection'].compact.join(separator)
+    end
+
+    def page_heading(title)
+        content_for(:title){ title }
+        content_tag(:h1, title)
+    end
 end
