8fc278f66ae47530981f4b5d532d625121dbcca3
Sean Wangnick <swangnick@gmail.com>
2021-09-26 12:59:15 -0500

    Contains features to create, read, update, and delete a book. This should realistically be split into four separate commits in the dev branch, merged into test, then merged into main.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_201353) do
+ActiveRecord::Schema.define(version: 2021_09_25_223942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 2021_09_22_201353) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
+    t.integer "price"
+    t.date "published_date"
   end
 
 end
